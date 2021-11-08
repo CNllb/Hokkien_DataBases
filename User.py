ffa7e81fd286afb5344ca21650e26895afdc3db2
CNllb <1360602885@qq.com>
2021-11-08 16:01:02 +0800

    【update】User的增删改函数

--- User.py
+++ User.py
@@ -1,26 +1,49 @@
+# 使用样例
+
+# 查找单个用户的信息
+# User.getUserInfo(userId)
+
+# 查找全部用户信息
+# User.getUserInfo()
+
+# 插入用户信息
+# User.insertUserInfo(userId,phoneNumber,userName,profilePicture = '',userMarks = '',
+# userWorks = '',userSearchRecord = '',userPrefer=''):
+
+# 更换用户头像
+# User.updateprofilePicture(userId,newProfilePicture)
+
+# 更换用户名
+# User.updateUserName(userId,newUserName)
+
+# 更换用户手机号码
+# User.updatephoneNumber(userId,newphoneNumber)
+
 import pymysql
 import json
-
-conn = pymysql.connect(
-        host="gz-cynosdbmysql-grp-56sj4bjz.sql.tencentcdb.com",
-        user="root",
-        port=25438,
-        password="Lcx010327",
-        database="Hokkien");
-
-# 创建游标
-cursor = conn.cursor();
 
 class User:
 
     # 获取单个用户信息
-    def getUserInfo(userId):
+    def getSingleUserInfo(userId):
+        conn = pymysql.connect(
+            host="gz-cynosdbmysql-grp-56sj4bjz.sql.tencentcdb.com",
+            user="root",
+            port=25438,
+            password="Lcx010327",
+            database="Hokkien");
+
+        # 创建游标
+        cursor = conn.cursor();
+
         sql = "SELECT * FROM User WHERE UserId = \"" + userId +"\";";
         try:
             # 执行SQL语句
             cursor.execute(sql),
             # 获取所有记录列表
             results = cursor.fetchall()
+            cursor.close()
+            conn.close()
             print(results)
             jsonData = []
             for row in results:
@@ -44,12 +67,23 @@ class User:
 
     # 获取所有用户信息
     def getUserInfo():
+        conn = pymysql.connect(
+            host="gz-cynosdbmysql-grp-56sj4bjz.sql.tencentcdb.com",
+            user="root",
+            port=25438,
+            password="Lcx010327",
+            database="Hokkien");
+
+        # 创建游标
+        cursor = conn.cursor();
         sql = "SELECT * FROM User";
         try:
             # 执行sql语句
             cursor.execute(sql)
             # 获取所有用户信息
             results = cursor.fetchall()
+            cursor.close()
+            conn.close()
             jsonData = []
             for row in results:
                 result = {}
@@ -71,7 +105,17 @@ class User:
             return jsondatar[1:len(jsondatar) - 1]
 
     # 插入用户信息
-    def insertUserInfo(userId,phoneNumber,userName,profilePicture = '',userMarks = '',userWorks = '',userSearchRecord = '',userPrefer=''):
+    def insertUserInfo(userId,phoneNumber,userName,profilePicture = '',userMarks = None,userWorks = None,userSearchRecord = None,userPrefer=None):
+        conn = pymysql.connect(
+            host="gz-cynosdbmysql-grp-56sj4bjz.sql.tencentcdb.com",
+            user="root",
+            port=25438,
+            password="Lcx010327",
+            database="Hokkien");
+
+        # 创建游标
+        cursor = conn.cursor();
+
         sql =  "INSERT INTO User(userId,phoneNumber,userName,profilePicture,userMarks,userWorks,userSearchRecord,userPrefer) \
         VALUES('%s','%s','%s','%s','%s','%s','%s','%s')"% \
         (userId,phoneNumber,userName,profilePicture,userMarks,userWorks,userSearchRecord,userPrefer);
@@ -80,10 +124,129 @@ class User:
             cursor.execute(sql);
             # 执行sql语句
             conn.commit();
+            cursor.close()
+            conn.close()
         except:
             print("Error: unable to insert data")
 
+    # 更换用户头像
+    def updateprofilePicture(userId,newProfilePicture):
+        conn = pymysql.connect(
+            host="gz-cynosdbmysql-grp-56sj4bjz.sql.tencentcdb.com",
+            user="root",
+            port=25438,
+            password="Lcx010327",
+            database="Hokkien");
+
+        # 创建游标
+        cursor = conn.cursor();
+        sql = "UPDATE User SET profilePicture = \"" + newProfilePicture + "\" WHERE userId = \"" + userId + "\";";
+        try:
+            # 执行sql语句
+            cursor.execute(sql)
+            # 执行mysql语句
+            conn.commit()
+            cursor.close()
+            conn.close()
+        except:
+            print("Error: unable to update profilePicture")
+
+    # 更换用户名
+    def updateUserName(userId,newUserName):
+        conn = pymysql.connect(
+            host="gz-cynosdbmysql-grp-56sj4bjz.sql.tencentcdb.com",
+            user="root",
+            port=25438,
+            password="Lcx010327",
+            database="Hokkien");
+
+        # 创建游标
+        cursor = conn.cursor();
+        sql = "UPDATE User SET userName = \""+newUserName+"\""+"WHERE userId = \"" + userId + "\";";
+        try:
+            # 执行sql语句
+            cursor.execute(sql)
+            # 执行sql语句
+            conn.commit()
+            cursor.close()
+            conn.close()
+        except:
+            print("Error: unable to update userName")
+
+    # 更换用户手机号码
+    def updatephoneNumber(userId,newphoneNumber):
+        conn = pymysql.connect(
+            host="gz-cynosdbmysql-grp-56sj4bjz.sql.tencentcdb.com",
+            user="root",
+            port=25438,
+            password="Lcx010327",
+            database="Hokkien");
+
+        # 创建游标
+        cursor = conn.cursor();
+
+        sql = "UPDATE User SET phoneNumber = \""+newphoneNumber+"\""+" WHERE userId = \"" + userId + "\";";
+        try:
+            # 执行sql语句
+            cursor.execute(sql)
+            # 执行sql语句
+            conn.commit()
+        except:
+            print("Error: unable to update phoneNumber")
+        cursor.close()
+        conn.close()
+
+    # 查找用户收藏记录
+    def getuserMarks(userId):
+        conn = pymysql.connect(
+            host="gz-cynosdbmysql-grp-56sj4bjz.sql.tencentcdb.com",
+            user="root",
+            port=25438,
+            password="Lcx010327",
+            database="Hokkien");
+
+        # 创建游标
+        cursor = conn.cursor();
+        sql = "SELECT userMarks FROM User WHERE userId = \""+userId+"\";";
+
+        try:
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            cursor.close()
+            conn.close()
+            for row in result:
+                userMarks = row[0]
+            userMarks = userMarks.split(",")
+            if userMarks[0] == '':
+                return []
+            return userMarks
+        except:
+            print("Error: unable to fetch userMarks")
+
+    # 添加用户收藏记录
+    def insertuserMarks(userId,workId):
+        conn = pymysql.connect(
+            host="gz-cynosdbmysql-grp-56sj4bjz.sql.tencentcdb.com",
+            user="root",
+            port=25438,
+            password="Lcx010327",
+            database="Hokkien");
+
+        # 创建游标
+        cursor = conn.cursor();
+        sql_fetch = "SELECT userMarks FROM User WHERE userId = \""+userId+"\";";
+        try:
+            cursor.execute(sql_fetch)
+            result = cursor.fetchall()
+            cursor.close()
+            conn.close()
+            for row in result:
+                userMarks = row[0]
+            userMarks = userMarks.split(",")
+            userMarks.append(workId)
+            print(userMarks)
+        except:
+            print("Error: unable to update userMarks")
 
 if __name__ == "__main__":
-    jsonData = User.getUserInfo()
-    print(jsonData)
+    User.insertuserMarks("7","1615")
